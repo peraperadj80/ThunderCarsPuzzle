c81b39fdc0527062e56a8ebd7738edadf2f20bd5
Petar Djordjevic <petar@Petars-MacBook-Air.local>
2017-05-01 19:03:30 +0200

    Initial Commit

---
+++ munja puzzle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  munja puzzle
+//
+//  Created by Petar Djordjevic on 5/1/17.
+//  Copyright © 2017 g11. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
